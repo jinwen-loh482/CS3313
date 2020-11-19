22c5d1fcd60b652bb6ed3d889a00a7a2b2d4160d
jinwen-loh482 <jwl482@nyu.edu>
2020-11-19 01:58:19 -0500

    Project 5

---
+++ Projects/Project 5/SDLProject/Level1.cpp
@@ -0,0 +1,117 @@
+#include "Level1.h"
+#define LEVEL1_WIDTH 26
+#define LEVEL1_HEIGHT 9
+
+#define LEVEL1_ENEMY_COUNT 1
+
+unsigned int level1_data[] =
+{
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,     1, 1, 1, 1, 1, 1, 1, 1, 1, 1,   3, 3, 3, 1,
+    3, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,     2, 2, 2, 2, 2, 2, 2, 2, 2, 2,   3, 3, 3, 2,
+    3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,     2, 2, 2, 2, 2, 2, 2, 2, 2, 2,   3, 3, 3, 2
+};
+
+void Level1::Initialize(int lives) {
+    state.livesLeft = lives;
+    state.sceneNumber = 1;
+    state.nextScene = -1;
+    state.control = IN_PROGRESS;
+    GLuint mapTextureID = Util::LoadTexture("Assets/Textures/tileset.png");
+    fontTextureID = Util::LoadTexture("Assets/Textures/font1.png");
+    state.map = new Map(LEVEL1_WIDTH, LEVEL1_HEIGHT, level1_data, mapTextureID, 1.0f, 4, 1);
+    // Move over all of the player and enemy code from initialization.
+    // Initialize Player
+    state.player = new Entity();
+    state.player->entityType = PLAYER;
+    state.player->position = glm::vec3(5, -5, 0);
+    state.player->movement = glm::vec3(0);
+    state.player->acceleration = glm::vec3(0, -9.81f, 0);
+    state.player->speed = 3.0f;
+    state.player->textureID = Util::LoadTexture("Assets/Textures/george_0.png");
+    
+    state.player->animRight = new int[4] {3, 7, 11, 15};
+    state.player->animLeft = new int[4] {1, 5, 9, 13};
+    state.player->animUp = new int[4] {2, 6, 10, 14};
+    state.player->animDown = new int[4] {0, 4, 8, 12};
+
+    state.player->animIndices = state.player->animRight;
+    state.player->animFrames = 4;
+    state.player->animIndex = 0;
+    state.player->animTime = 0;
+    state.player->animCols = 4;
+    state.player->animRows = 4;
+    
+    state.player->height = 0.8f;
+    state.player->width = 0.8f;
+    
+    state.player->jumpPower = 6.0f;
+    
+    
+    state.enemies = new Entity[LEVEL1_ENEMY_COUNT];
+    GLuint enemyTextureID = Util::LoadTexture("Assets/Textures/ctg.png");
+    
+    state.enemies[0].entityType = ENEMY;
+    state.enemies[0].textureID = enemyTextureID;
+    state.enemies[0].position = glm::vec3(15, 0, 0);
+    state.enemies[0].speed = 1;
+    state.enemies[0].acceleration = glm::vec3(0, -9.81f, 0);
+    state.enemies[0].aiType = WALKER;
+    state.enemies[0].aiState = ACTIVE;
+    state.enemies[0].isActive = true;
+}
+void Level1::Update(float deltaTime) {
+    if (state.control == IN_PROGRESS) {
+        state.player->Update(deltaTime, state.player, state.enemies, LEVEL1_ENEMY_COUNT, state.map, state.sceneNumber);
+        for (int i = 0; i < LEVEL1_ENEMY_COUNT; ++i) {
+            Entity* enemy = &state.enemies[i];
+            enemy->Update(deltaTime, state.player, state.enemies, LEVEL1_ENEMY_COUNT, state.map, state.sceneNumber);
+        }
+
+        if (state.player->position.x >= 23) {
+            state.nextScene = 2;
+            return;
+        }
+        if (!state.player->isActive) {
+            state.control = GAME_LOST;
+            if (state.livesLeft > 0) {
+                state.livesLeft--;
+                state.control = IN_PROGRESS;
+                Initialize(state.livesLeft);
+            }
+        }
+    }
+}
+void Level1::Render(ShaderProgram *program) {
+    state.map->Render(program);
+    state.player->Render(program);
+    for (int i = 0; i < LEVEL1_ENEMY_COUNT; ++i) {
+        Entity* enemy = &state.enemies[i];
+        enemy->Render(program);
+    }
+    glm::vec3 text_loc = state.player->position;
+    text_loc.x -= .75;
+    text_loc.y += .75;
+    glm::vec3 announce_loc = text_loc;
+    announce_loc.y += 1.5;
+    switch(state.livesLeft) {
+        case 2:
+            Util::DrawText(program, fontTextureID, "Lives Left: 2", 0.25f, -.1, text_loc);
+            break;
+        case 1:
+            Util::DrawText(program, fontTextureID, "Lives Left: 1", 0.25f, -.1, text_loc);
+            break;
+        case 0:
+            Util::DrawText(program, fontTextureID, "Lives Left: 0", 0.25f, -.1, text_loc);
+            break;
+    }
+    if (state.control == GAME_LOST)
+        Util::DrawText(program, fontTextureID, "You Lose!", 0.25f, -.1, announce_loc);
+    else if (state.control == GAME_WON)
+        Util::DrawText(program, fontTextureID, "You Win!", 0.25f, -.1, announce_loc);
+}
--- Projects/Project 5/SDLProject/main.cpp
+++ Projects/Project 5/SDLProject/main.cpp
@@ -7,16 +7,20 @@
 #define GL_GLEXT_PROTOTYPES 1
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include <SDL_mixer.h>
 #include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
-#include "Library/ShaderProgram.h"
+#include "ShaderProgram.h"
 
-#include "Library/Util.h"
-#include "Library/Entity.h"
-#include "Library/Map.h"
-#include "Library/Scene.h"
-#include "Library/Level1.h"
-#include "Library/Level2.h"
+
+#include "Util.h"
+#include "Entity.h"
+#include "Map.h"
+#include "Scene.h"
+#include "Level0.h"
+#include "Level1.h"
+#include "Level2.h"
+#include "Level3.h"
 
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
@@ -26,16 +30,22 @@ glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
 
 // Add some variables and SwitchToScene function
 Scene *currentScene;
-Scene *sceneList[2];
+Scene *sceneList[4];
 
-void SwitchToScene(Scene *scene) {
+void SwitchToScene(Scene *prev, Scene *scene) {
     currentScene = scene;
-    currentScene->Initialize();
+    if (prev != nullptr)
+        currentScene->Initialize(prev->state.livesLeft);
+    else
+        currentScene->Initialize(2);
 }
 
+Mix_Music *music;
+Mix_Chunk *jumpSound;
+
 void Initialize() {
-    SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("Platformer!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    displayWindow = SDL_CreateWindow("WITNESS ME!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
     
@@ -46,6 +56,15 @@ void Initialize() {
     glViewport(0, 0, 640, 480);
     
     program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+    
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096);
+    // For testing if the audio loops (and actually being able to hear jump sound effects)
+    // music = Mix_LoadMUS("Assets/Audio/Monkey.mp3");
+    // For the authentic MadMax Experience
+    music = Mix_LoadMUS("Assets/Audio/Valhalla.mp3");
+    Mix_PlayMusic(music, -1);
+    
+    jumpSound = Mix_LoadWAV("Assets/Audio/bounce.wav");
     
     viewMatrix = glm::mat4(1.0f);
     modelMatrix = glm::mat4(1.0f);
@@ -61,15 +80,16 @@ void Initialize() {
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
-    sceneList[0] = new Level1();
-    sceneList[1] = new Level2();
-    SwitchToScene(sceneList[0]);
+    
+    sceneList[0] = new Level0();
+    sceneList[1] = new Level1();
+    sceneList[2] = new Level2();
+    sceneList[3] = new Level3();
+    SwitchToScene(nullptr, sceneList[0]);
 }
 
 void ProcessInput() {
-    
     currentScene->state.player->movement = glm::vec3(0);
-    
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
@@ -80,40 +100,42 @@ void ProcessInput() {
                 
             case SDL_KEYDOWN:
                 switch (event.key.keysym.sym) {
-                    case SDLK_LEFT:
-                        // Move the player left
-                        break;
-                        
-                    case SDLK_RIGHT:
-                        // Move the player right
-                        break;
-                        
                     case SDLK_SPACE:
-                        if (currentScene->state.player->collidedBottom) currentScene->state.player->jump = true;
-                        break;
+                        if (currentScene->state.control == IN_PROGRESS) {
+                            if (currentScene->state.player->collidedBottom) {
+                                currentScene->state.player->jump = true;
+                                Mix_PlayChannel(-1, jumpSound, 0);
+                            }
+                            break;
+                        }
+                    case SDLK_RETURN:
+                        if (currentScene->state.sceneNumber == 0)
+                            currentScene->state.control = IN_PROGRESS;
+                            break;
                 }
                 break; // SDL_KEYDOWN
         }
     }
     
-    const Uint8 *keys = SDL_GetKeyboardState(NULL);
+    if (currentScene->state.control == IN_PROGRESS) {
+        const Uint8 *keys = SDL_GetKeyboardState(NULL);
 
-    if (keys[SDL_SCANCODE_LEFT]) {
-        currentScene->state.player->movement.x = -1.0f;
-        currentScene->state.player->animIndices = currentScene->state.player->animLeft;
-    }
-    else if (keys[SDL_SCANCODE_RIGHT]) {
-        currentScene->state.player->movement.x = 1.0f;
-        currentScene->state.player->animIndices = currentScene->state.player->animRight;
-    }
-    
+        if (keys[SDL_SCANCODE_LEFT]) {
+            currentScene->state.player->movement.x = -1.0f;
+            currentScene->state.player->animIndices = currentScene->state.player->animLeft;
+        }
+        else if (keys[SDL_SCANCODE_RIGHT]) {
+            currentScene->state.player->movement.x = 1.0f;
+            currentScene->state.player->animIndices = currentScene->state.player->animRight;
+        }
+        
 
-    if (glm::length(currentScene->state.player->movement) > 1.0f) {
-        currentScene->state.player->movement = glm::normalize(currentScene->state.player->movement);
+        if (glm::length(currentScene->state.player->movement) > 1.0f) {
+            currentScene->state.player->movement = glm::normalize(currentScene->state.player->movement);
+        }
     }
-
 }
-
+    
 #define FIXED_TIMESTEP 0.0166666f
 float lastTicks = 0.0f;
 float accumulator = 0.0f;
@@ -137,10 +159,12 @@ void Update() {
     accumulator = deltaTime;
     
     viewMatrix = glm::mat4(1.0f);
-    if (currentScene->state.player->position.x > 5) {
-        viewMatrix = glm::translate(viewMatrix,glm::vec3(-currentScene->state.player->position.x, 3.75, 0));
-    } else {
-        viewMatrix = glm::translate(viewMatrix, glm::vec3(-5, 3.75, 0));
+    if (currentScene->state.sceneNumber != 0) {
+        if (currentScene->state.player->position.x > 5) {
+            viewMatrix = glm::translate(viewMatrix,glm::vec3(-currentScene->state.player->position.x, 3.75, 0));
+        } else {
+            viewMatrix = glm::translate(viewMatrix, glm::vec3(-5, 3.75, 0));
+        }
     }
 }
 
@@ -165,7 +189,7 @@ int main(int argc, char* argv[]) {
         ProcessInput();
         Update();
         
-        if (currentScene->state.nextScene >= 0) SwitchToScene(sceneList[currentScene->state.nextScene]);
+        if (currentScene->state.nextScene >= 0) SwitchToScene(currentScene, sceneList[currentScene->state.nextScene]);
         
         Render();
     }
